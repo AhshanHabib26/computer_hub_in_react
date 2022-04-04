d5853f0ed6171e5ae04c5baaf92676f55184db87
AhshanHabib26 <habib.net27@gmail.com>
2022-04-04 10:28:15 +0600

    About Page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h1>About Page</h1>
+        <div className='about_component'>
+            <h1>About Page!</h1>
+            <p>Welcome! My Latest Computer Reviews Hub. Here You Can Find Out All Of Special And Best Computer Accessories. Stay With Us, And Also Support..........</p>
         </div>
     );
 };
